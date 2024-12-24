e3b61d7e6a2c19b51cb708edd7c34bcd9a9750bb
onion <a0966550619@gmail.com>
2024-12-24 15:36:01 +0800

    補充在後端取得班級代碼回傳及增加頁面載入時的預設班級

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -3,8 +3,11 @@ include_once "db.php";
 //處理刪除資料的請求
 $id=$_POST['id'];
 
+//$row=$Stu->find($id);
+
 $Stu->del($id);
 // echo $Stu->del($id);
 
 echo $_POST['classroom'];
+// echo $row['classroom'];
 ?>
