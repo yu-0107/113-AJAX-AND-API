50abd1c60527c6600649ac3ff0ecf87e6c44bd21
onion <a0966550619@gmail.com>
2024-12-27 10:58:10 +0800

    使用ajax 載入modal,並以表單傳送新增資料

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -1,5 +1,8 @@
 <?php
+include_once "db.php";
 //處理新增資料的請求
+$Stu->save($_POST);
 
+to("../index.html");
 
 ?>
--- app2/create_form.php
+++ app2/create_form.php
@@ -2,7 +2,7 @@
 <div class="modal fade" id="CreateModal" tabindex="-1" aria-labelledby="CreateModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form action="api/create.php" method="post">
+            <form action="api/insert.php" method="post">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="CreateModalLabel">新增學生</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -10,28 +10,28 @@
                 <div class="modal-body">
                     <div class="mb-3">
                         <label for="uni_id" class="form-label">學號</label>
-                        <input type="text" class="form-control" id="uni_id">
+                        <input type="text" name="uni_id" class="form-control" id="uni_id">
                     </div>
                     <div class="mb-3">
                         <label for="seat_num" class="form-label">座號</label>
-                        <input type="text" class="form-control" id="seat_num">
+                        <input type="text" name="seat_num" class="form-control" id="seat_num">
                     </div>
                     <div class="mb-3">
                         <label for="name" class="form-label">姓名</label>
-                        <input type="text" class="form-control" id="name">
+                        <input type="text" name="name" class="form-control" id="name">
                     </div>
                     <div class="mb-3">
                         <label for="classroom" class="form-label">班級</label>
-                        <input type="text" class="form-control" id="classroom">
+                        <input type="text" name="classroom" class="form-control" id="classroom">
                     </div>
                     <div class="mb-3">
                         <label for="major" class="form-label">科系</label>
-                        <input type="text" class="form-control" id="major">
+                        <input type="text" name="major" class="form-control" id="major">
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
                     <button type="submit" class="btn btn-primary">儲存</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
                 </div>
             </form>
         </div>
